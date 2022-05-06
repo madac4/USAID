bbe1f5a797dac8f1d2298b232f8ad40712eefa27
Petru Orbu <whhroo@Petrus-MacBook-Pro.local>
2022-05-06 12:47:31 +0300

    header

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import * as flsFunctions from './modules/functions.js'
+
+flsFunctions.isWebp();
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const burger = document.querySelector('.burger');
+    const header = document.querySelector('.header');
+    const menu = document.querySelector('.header-nav');
+    flsFunctions.burger(burger, menu, header)
+})

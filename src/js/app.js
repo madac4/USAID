40e9e9ad9aa31a5126ab4e6c5d1629e4adcf139e
Petru Orbu <whhroo@Petrus-MacBook-Pro.local>
2022-05-11 15:45:41 +0300

    final-front

--- src/js/app.js
+++ src/js/app.js
@@ -118,4 +118,68 @@ document.addEventListener('DOMContentLoaded', () =>{
             }
         })
     }
+
+    const partnerSliders = document.querySelectorAll('.slider-logos__body');
+
+    if (partnerSliders.length >= 1) {
+        for (let i = 0; i < partnerSliders.length; i++) {
+            partnerSliders[i].classList.add(`slider-logos__body--${i}`);
+            partnerSliders[i].parentNode.querySelector('.slider-logos-controls__arrows').classList.add(`slider-logos-${i}-controls__arrows`);
+
+            new Swiper(`.slider-logos__body--${i}`, {
+                grabCursor: true,
+                observer: true,
+                observeParents: true,
+                speed: 800,
+                // spaceBetween: 20,
+                preloadImages: false,
+                slidesPerView: 5,
+                lazy: true,
+                navigation: {
+                    prevEl: `.slider-logos-${i}-controls__arrows .slider-arrow__prev`,
+                    nextEl: `.slider-logos-${i}-controls__arrows .slider-arrow__next`,
+                },
+                breakpoints: {
+                    320:{
+                        slidesPerView: 2,
+                    },
+                    480:{
+                        slidesPerView: 3,
+                    },
+                    767: {
+                        slidesPerView: 4,
+                    },
+                    992: {
+                        slidesPerView: 5,
+                    },
+                },
+            })
+
+        }
+    }
+
+    const tabs = document.querySelector('.tabs');
+    const tabsButtons = document.querySelectorAll('.tabs__button');
+    const tabsContents = document.querySelectorAll('.tabs__content');
+    
+    if (tabs) {
+        tabs.addEventListener('click', (e) => {
+            if (e.target.classList.contains('tabs__button')) {
+                const tabsPath = e.target.dataset.tabsPath;
+                tabsHandler(tabsPath);
+            }
+        })
+    }
+    
+    const tabsHandler = (path) => {
+        tabsButtons.forEach(button => {
+            button.classList.remove('tabs__button--active')
+        })
+        document.querySelector(`[data-tabs-path="${path}"]`).classList.add('tabs__button--active');
+    
+        tabsContents.forEach(content => {
+            content.classList.remove('tabs__content--active')
+        })
+        document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs__content--active');
+    }
 })
